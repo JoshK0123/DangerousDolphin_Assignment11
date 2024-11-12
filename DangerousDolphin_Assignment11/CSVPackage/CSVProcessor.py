5bb93d465ccb55f514d688682789dc266408960c
smai-ley <tkrkh43@gmail.com>
2024-11-12 09:04:45 -0500

    Added teammember documentation

--- DangerousDolphin_Assignment11/CSVPackage/CSVProcessor.py
+++ DangerousDolphin_Assignment11/CSVPackage/CSVProcessor.py
@@ -1,3 +1,15 @@
+# Name: Sarah Mahan, Luke Ransick, Josh Klingelhafer, Henry Gruber
+# email:  mahansa@mail.uc.edu, ransiclg@mail.uc.edu, klingejh@mail.uc.edu, gruberhw@mail.uc.edu
+# Assignment Number: Assignment 11
+# Due Date:   11/21/24
+# Course #/Section:   IS4010 001
+# Semester/Year:   Fall 2024
+# Brief Description of the assignment:  Clean a csv file, store it in a new place.
+
+# Brief Description of what this module does. {Do not copy/paste from a previous assignment. Put some thought into this. required}
+# Citations:
+# Anything else that's relevant: Using (allowed) AI
+
 # CSVProcessor.py
 # Bill Nicholson
 # nicholdw@ucmail.uc.edu
